1241dc17d0a167b1a4b32e4174bb9e45c28ebff1
Jay-Parmar <jay.parmar@joshtechnologygroup.com>
2021-09-14 20:14:02 +0530

    Added docstrings in user views and test cases for user

--- There4U/user/tests.py
+++ There4U/user/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import User
 
-# Create your tests here.
+class UserTests(APITestCase):
+
+    def setUp(self):
+        self.user_data = {
+            'name': 'heyjay',
+            'email': 'heyjay@gmail.com',
+            'city': 'raipur',
+            'state': 'cg',
+            'zip_code': 495001
+        }
+        self.response = self.client.post(reverse('user-list'), self.user_data, format='json')
+
+    def test_can_create_user(self):
+        """
+        Ensure we can create a new user object.
+        """
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().name, 'heyjay')
+
+    def test_can_read_user_list(self):
+        """
+        Ensure we can read all user objects
+        """
+        response = self.client.get(reverse('user-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_read_user_detail(self):
+        """
+        Ensure we can read a particular user details
+        """
+        id = User.objects.get().id
+        response = self.client.get(reverse('user-detail', args=[id]))
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_update_user(self):
+        """
+        Ensure we can update a user with given data
+        """
+        id = User.objects.get().id
+        test_data = {
+            'name': 'teyhey',
+            'email': 'teyhey@gmail.com',
+            'city': 'raipur',
+            'state': 'cg',
+            'zip_code': 495001
+        }
+        response = self.client.put(reverse('user-detail', args=[id]), test_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_delete_user(self):
+        """
+        Ensure we can read all user objects
+        """
+        id = User.objects.get().id
+        response = self.client.delete(reverse('user-detail', args=[id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- There4U/user/views.py
+++ There4U/user/views.py
@@ -11,22 +11,27 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
 
     def return_http_200(self, response):
+        '''Returns response with status code as 200.'''
         response.status_code = status.HTTP_200_OK
         return response
 
     def get_serializer_class(self):
+        '''Overriding class to return User without an id.'''
         if self.action in ["retrieve"]:
             return UserSerializerWithoutID
         return super().get_serializer_class()
 
     def create(self, request, *args, **kwargs):
+        '''Creating a new User.'''
         response = super().create(request, *args, **kwargs)
         return self.return_http_200(response)
 
     def update(self, request, *args, **kwargs):
+        '''Updating exing User.'''
         response = super().update(request, *args, **kwargs)
         return self.return_http_200(response)
 
     def destroy(self, request, *args, **kwargs):
+        '''Deleting a User'''
         response = super().destroy(request, *args, **kwargs)
         return self.return_http_200(response)
